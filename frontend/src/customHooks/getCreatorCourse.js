070ab21fcd4570f64711396f64eaf08463c1771b
Samra <samramuqadasvu1@gmail.com>
2026-04-12 15:21:17 +0500

    cretorcourse backend

--- frontend/src/customHooks/getCreatorCourse.js
+++ frontend/src/customHooks/getCreatorCourse.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {serverUrl} from  "../App"
+import axios from "axios"
 
 function getCreatorCourse() {
   return (
-    <div>
-      
-    </div>
+    useEffect(()=>{
+      const creatorCourses=async()=>{
+        try {
+          const result=await axios.get(serverUrl,+"/api/course/getcreator",{with})
+        } catch (error) {
+          
+        }
+      }
+    },[])
   )
 }
 
